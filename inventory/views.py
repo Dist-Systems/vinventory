b15e6d71cb4e352b41a1606d1bbddfb523c7c454
noah.de <noah.de@gmail.com>
2011-12-06 21:26:23 +0000

    Added DataStore to the template system, renamed templates and urls

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render_to_response
-from inventory.models import ISCNode
-# Create your views here.
+from inventory.models import *
+from django.template import Context, loader, RequestContext
+from django.http import HttpResponse
+
+
+def test(request):
+    latest_poll_list = IpAddress.objects.all().order_by('-address')[:5]
+    return render_to_response("index.html",  {'object_list': latest_poll_list},
+context_instance=RequestContext(request))	
--- urls.py
+++ urls.py
@@ -2,11 +2,15 @@ from django.conf.urls.defaults import *
 from django.views.generic import list_detail, create_update
 from inventory.models import *
 
-info_dict = {
+vm_dict = {
     # 'queryset':ISCNode.objects.all(),
 	'queryset':VirtualMachine.objects.order_by('name'),
 }
 
+dstore_dict = {
+    # 'queryset':ISCNode.objects.all(),
+	'queryset':DataStore.objects.order_by('name'),
+}
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -23,6 +27,8 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
 
-    url(r'^$', list_detail.object_list, dict(info_dict, template_name='index.html'), name='homepage'),
-    url(r'^system/(?P<object_id>[-\w]+)/', list_detail.object_detail, dict(info_dict, template_name='iscnode_detail.html'), name='system-display'),
+    url(r'^$', list_detail.object_list, dict(vm_dict, template_name='index.html'), name='homepage'),
+    url(r'^test/$', 'inventory.views.test'),
+    url(r'^system/(?P<object_id>[-\w]+)/', list_detail.object_detail, dict(vm_dict, template_name='vm_detail.html'), name='vm-detail'),
+	url(r'^dstore/(?P<object_id>[-\w]+)/', list_detail.object_detail, dict(dstore_dict, template_name='datastore.html'), name='datastore'),
 )
