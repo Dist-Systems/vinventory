69b2d366a823e71b8c4c135be38d0271c3c40819
noah.de@gmail.com <noah.de@gmail.com@d4db623b-45a0-a485-85cf-2895aaa505fa>
2011-12-14 23:50:27 +0000

    Continual refinement

--- trunk/inventory/views.py
+++ trunk/inventory/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from inventory.models import *
 from django.template import Context, loader, RequestContext
 from django.http import HttpResponse
@@ -6,4 +6,17 @@ from django.http import HttpResponse
 
 def test(request):
     latest_vendor_list = Vendor.objects.all()[:5]
-    return render_to_response("test.html",  {'object_list': latest_vendor_list}, context_instance=RequestContext(request))	
+    return render_to_response("test.html",  {'object_list': latest_vendor_list}, context_instance=RequestContext(request))
+
+def homepage(request):
+    vendor_list = Vendor.objects.all()
+    server_list = Server.objects.all()
+    vm_list = VirtualMachine.objects.all()
+    return render_to_response(
+	    "index.html",
+		{'vendor_list': vendor_list, 'server_list':server_list, 'vm_list':vm_list}, 
+		context_instance=RequestContext(request))
+
+def detail(request, server_id):
+    s = get_object_or_404(Server, pk=server_id)
+    return render_to_response('server_detail.html', {'server': s})
