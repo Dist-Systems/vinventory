386543dc46df7dff8ac9d5bcab2d3f7fd7b655b3
noah.de@gmail.com <noah.de@gmail.com@d4db623b-45a0-a485-85cf-2895aaa505fa>
2011-12-15 22:48:49 +0000

    Developing tests.py

--- trunk/inventory/tests.py
+++ trunk/inventory/tests.py
@@ -1,15 +1,56 @@
 #from django.utils import unittest
 import unittest
-from inventory.models import *
+from inventory.models import Server, DataStore, VMHost, VirtualMachine, Vendor, IpAddress
+from datetime import datetime
 
+# https://docs.djangoproject.com/en/dev/topics/testing/
 # https://docs.djangoproject.com/en/1.3/topics/testing/
+
+# CONSTANTS
+CAPACITYMB=1024
+FREESPACEMB=2
+CPUNUM=1
+
+# Utility methods defined here and used throughout the test cases
+class Util:
+    @staticmethod
+    def new_vendor(name, website=''):
+	  vendor = Vendor(name=name, website=website)
+	  vendor.save()
+	  return vendor
+
+    @staticmethod
+    def new_server(name, vendor):
+      notes = '[default notes]'
+      capMB = CAPACITYMB
+      cpuNum = CPUNUM
+      purchased = datetime.now()
+      server = Server(name=name, notes=notes, vendor=vendor, capacityMB=capMB, cpuCount=cpuNum, purchased=purchased)
+      server.save()
+      return server
+
+    @staticmethod
+    def new_datastore(name):
+      capacityMB=CAPACITYMB
+      freespaceMB=FREESPACEMB
+      filesystemVersion="3.14"
+      datastore = DataStore(name=name,capacityMB=capacityMB,freespaceMB=freespaceMB,filesystemVersion=filesystemVersion)
+      datastore.save()
+      return datastore
+
+    @staticmethod
+    def new_vmhost():
+	    pass
 
 class InventoryTestCase(unittest.TestCase):
     def setUp(self):
-        self.datastore1 = DataStore.objects.create(name="Storino",capacityMB=1024,freespaceMB=24,filesystemVersion="3.14")
+        self.vendor = Util.new_vendor(name='Vendorino', website='http://venderino.com')
+        self.datastore1 = Util.new_datastore("Storino")
+        self.server1 = Util.new_server('Serverino', self.vendor)
 
-    def testAttributes(self):
+    def testDataStoreAttributes(self):
+        print 'Asserting DataStore Attributes ...'
         self.assertEqual(self.datastore1.name, "Storino")
-        self.assertEqual(self.datastore1.capacityMB, 1024)
-        self.assertEqual(self.datastore1.freespaceMB, 24)
+        self.assertEqual(self.datastore1.capacityMB, CAPACITYMB)
+        self.assertEqual(self.datastore1.freespaceMB, FREESPACEMB)
         self.assertEqual(self.datastore1.filesystemVersion, "3.14")
