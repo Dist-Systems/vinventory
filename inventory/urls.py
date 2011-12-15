2fdec0f138f77f5232efdd373fc63345c145ddad
noah.de <noah.de@gmail.com>
2011-12-15 18:06:53 +0000

    Decoupled urls from the inventory application

---
+++ inventory/urls.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from django.conf.urls.defaults import *
+from django.views.generic import list_detail, create_update
+from inventory.models import *
+
+vm_dict = {
+	'queryset':VirtualMachine.objects.order_by('name'),
+}
+
+dstore_dict = {
+	'queryset':DataStore.objects.order_by('name'),
+}
+
+host_dict = {
+	'queryset':VMHost.objects.order_by('name'),
+}
+
+urlpatterns = patterns('',
+	url(r'^server/(?P<server_id>\d+)/$', 'inventory.views.server_detail', name='server-detail'),
+    url(r'^system/(?P<object_id>[-\w]+)/', list_detail.object_detail, dict(vm_dict, template_name='vm_detail.html'), name='vm-detail'),
+	url(r'^dstore/(?P<object_id>[-\w]+)/', list_detail.object_detail, dict(dstore_dict, template_name='datastore.html'), name='datastore'),
+	url(r'^host/(?P<object_id>[-\w]+)/', list_detail.object_detail, dict(host_dict, template_name='host.html'), name='host'),
+)
--- urls.py
+++ urls.py
@@ -1,46 +1,22 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
-from django.views.generic import list_detail, create_update
-from inventory.models import *
 
-vm_dict = {
-    # 'queryset':ISCNode.objects.all(),
-	'queryset':VirtualMachine.objects.order_by('name'),
-}
-
-dstore_dict = {
-    # 'queryset':ISCNode.objects.all(),
-	'queryset':DataStore.objects.order_by('name'),
-}
-
-host_dict = {
-    # 'queryset':ISCNode.objects.all(),
-	'queryset':VMHost.objects.order_by('name'),
-}
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^proj/', include('proj.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
+
     # index: homepage
-    #url(r'^$', list_detail.object_list, dict(vm_dict, template_name='index.html'), name='homepage'),
     url(r'^$', 'inventory.views.homepage', name='homepage'),
 	
-	url(r'^test/$', 'inventory.views.test'),
-	url(r'^server/(?P<server_id>\d+)/$', 'inventory.views.detail'),
-    url(r'^system/(?P<object_id>[-\w]+)/', list_detail.object_detail, dict(vm_dict, template_name='vm_detail.html'), name='vm-detail'),
-	url(r'^dstore/(?P<object_id>[-\w]+)/', list_detail.object_detail, dict(dstore_dict, template_name='datastore.html'), name='datastore'),
-	url(r'^host/(?P<object_id>[-\w]+)/', list_detail.object_detail, dict(host_dict, template_name='host.html'), name='host'),
+	# inventory urls
+	url(r'^inventory/', include('inventory.urls')),
+
 	
 	# Static media 
 	url(r'^site_media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT,'show_indexes': True }),
