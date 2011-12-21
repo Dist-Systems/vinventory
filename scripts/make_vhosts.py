90a9c0e5bed4101e2968e4f24c343c8a1a6878d9
noah.de <noah.de@gmail.com>
2011-12-21 06:44:35 +0000

    make scripts now use DictReader, adding to the readability of the code

--- scripts/make_vhosts.py
+++ scripts/make_vhosts.py
@@ -4,43 +4,53 @@ import sys,os,csv
 path = os.path.abspath('.')
 
 # Full path and name to your csv file
-csv_filepathname=path+"/data/VMHost.csv"
+csv_filepathname=path+'/data/VMHost.csv'
 
 sys.path.append(path)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 from inventory.models import VMHost, Vendor
 
-import csv
-dataReader = csv.reader(open(csv_filepathname))
-Vendors = Vendor.objects.filter()
+# Define our field names, since the file doesn't match our model
+names = ('Name','Manufacturer','Model','NumCpu','CpuTotalMhz','CpuUsageMhz','ProcessorType')
+dataReader = csv.DictReader(open(csv_filepathname), fieldnames=names)
+
+# Don't need the header, because the names don't match
+dataReader.next()
+
+# For every line in the file (lines represent Virtual Machine Hosts)
 for row in dataReader:
-  if  not (row[0].startswith('#') or (row[0] == "Name")):
-    print row[1],
-    if not Vendor.objects.filter(name=row[1]):
+    print row['Manufacturer'],
+    
+    # Seek an existing vendor (since vendor is a requirement)
+    if not Vendor.objects.filter(name=row['Manufacturer']):
         # create vendor
         v  =  Vendor()
-        v.name = row[1]
+        v.name = row['Manufacturer']
         v.save()
         print ' ...created'
     else:
-        v  = Vendor.objects.get(name=row[1])
-        print ' ...exists'
-
-    print row[0],
-
-    if not VMHost.objects.filter(name=row[0]):
-        vmh      = VMHost()
-        vmh.name = row[0]
-        print ' ...created'
-    else:
-        vmh = VMHost.objects.get(name=row[0])
+        v  = Vendor.objects.get(name=row['Manufacturer'])
         print ' ...exists'
     
+    print row['Name'],
+    
+    # Seek out an existing Virtual Machine Host
+    if not VMHost.objects.filter(name=row['Name']):
+        # If it does not exist, create one and specify the name
+        vmh      = VMHost()
+        vmh.name = row['Name']
+        print ' ...created'
+    else:
+        # If it exists, grab it and assign it to vmh variable
+        vmh = VMHost.objects.get(name=row['Name'])
+        print ' ...exists'
+
+    # We can now update the object and save it
     vmh.manufacturer = v
-    vmh.model        = row[2]
-    vmh.cpuCount     = int(row[3])
-    vmh.cpuTotal     = int(row[4])
-    vmh.cpuUsage     = row[5]
-    vmh.processor    = row[6]
+    vmh.model        = row['Model']
+    vmh.cpuCount     = int(row['NumCpu'])
+    vmh.cpuTotal     = int(row['CpuTotalMhz'])
+    vmh.cpuUsage     = row['CpuUsageMhz']
+    vmh.processor    = row['ProcessorType']
     vmh.save()
 print 'updated'
