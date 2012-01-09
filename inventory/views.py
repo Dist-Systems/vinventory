7c532a9154d84ff779e06c448eb9eba474717647
noah.de <noah.de@gmail.com>
2012-01-09 20:03:08 +0000

    refinement of server creation and web viewing

--- inventory/views.py
+++ inventory/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render_to_response, get_object_or_404, redirect
-from django.core.context_processors import csrf
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import Context, loader, RequestContext
 from django.http import HttpResponse, HttpResponseRedirect
-from django.core.urlresolvers import reverse
+
 from inventory.forms import NewServerForm
 from inventory.models import *
 
@@ -23,20 +22,17 @@ def server_detail(request, server_id):
     s = get_object_or_404(Server, pk=server_id)
     return render_to_response('server_detail.html', {'server': s})
 
-def newServer(request):
-    vendor_list = Vendor.objects.all()
-    server_list = Server.objects.all()
-    vm_list = VirtualMachine.objects.all()
-    
-    if request.method == 'POST': # If the form has been submitted...
-        form = NewServerForm(request.POST) # A form bound to the POST data
-
-        if form.is_valid(): # All validation rules pass
+def newServer(request): 
+    # If the form has been submitted...
+    if request.method == 'POST': 
+        # A form bound to the POST data
+        form = NewServerForm(request.POST)
+        # All validation rules pass
+        if form.is_valid(): 
             form.save()
-            # Process the data in form.cleaned_data
-            # ...
-            return HttpResponseRedirect('/') # Redirect after POST
-            #return redirect('index.html')
+            # Redirect after POST
+            return HttpResponseRedirect('/') 
     else:
-        form = NewServerForm() # An unbound form
+        # An unbound form
+        form = NewServerForm() 
     return render_to_response('newServer.html', { 'form': form,},  context_instance=RequestContext(request))
