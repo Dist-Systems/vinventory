9cbfa4c422c04baa82aee62a06250f958ecc5ede
noah.de@gmail.com <noah.de@gmail.com@d4db623b-45a0-a485-85cf-2895aaa505fa>
2012-01-18 23:02:54 +0000

    Many changes to models which had to be reflected in the templates. New form for editing servers. CSS bar graph to show datastore usage in the list view.

--- trunk/inventory/forms.py
+++ trunk/inventory/forms.py
@@ -5,3 +5,7 @@ from inventory.models import Server
 class NewServerForm(forms.ModelForm):
     class Meta:
         model = Server
+
+class EditServerForm(forms.ModelForm):
+    class Meta:
+        model = Server
