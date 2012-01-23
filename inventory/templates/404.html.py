899f0ab3f0915b0b919e94d35b71a68c3a14c49d
noah.de <noah.de@gmail.com>
2012-01-23 04:17:20 +0000

    Real unit testing, using named fixtures

---
+++ inventory/templates/404.html.py
@@ -0,0 +1,9 @@
+{% extends "base.html" %}
+
+{% load pagination_tags %}
+{% block title %}About{% endblock title%}
+
+{% block content %}
+   <h1>404 error</h1>
+   <p>Not found</p>
+{% endblock content%}
--- inventory/tests.py
+++ inventory/tests.py
@@ -1,72 +1,27 @@
-#from django.utils import unittest
-#import unittest
-import os
 from django.test import TestCase
-from django.core.management import call_command
-from inventory.models import Server, DataStore, VMHost, VirtualMachine, Vendor, IpAddress
-from inventory.utils import createDataStores, createVirtualHosts, createVirtualMachines, createIPs, openDataSource
-from datetime import datetime
+from inventory.models import *
 
-# https://docs.djangoproject.com/en/dev/topics/testing/
-# https://docs.djangoproject.com/en/1.3/topics/testing/
+class InventoryViewsTestCase(TestCase):
+    '''
+    Class variable to load a specific fixture 
+    file for this set of tests
+    '''
+    fixtures = ['inventory_test']
 
-# CONSTANTS
-CAPACITYMB=1024
-FREESPACEMB=2
-CPUNUM=1
-DATETIME = datetime(2011, 12, 15, 21, 2, 34, 863905)
-
-# Utility methods defined here and used throughout the test cases
-class Util:
-    @staticmethod
-    def new_vendor(name, website=''):
-      vendor = Vendor(name=name, website=website)
-      vendor.save()
-      return vendor
-
-    @staticmethod
-    def new_server(name, vendor):
-      notes = '[default notes]'
-      capMB = CAPACITYMB
-      cpuNum = CPUNUM
-      purchased = DATETIME
-      server = Server(name=name, notes=notes, vendor=vendor, capacityMB=capMB, cpuCount=cpuNum, purchased=purchased)
-      server.save()
-      return server
-
-    @staticmethod
-    def new_datastore(name):
-      ds = {}
-      ds['name']             = name
-      ds['capacityMB']       = CAPACITYMB
-      ds['freespaceMB']      = FREESPACEMB
-      ds['filesystemVersion']= "3.14"
-      datastore = DataStore.create(**ds)
-      datastore.save()
-      return datastore
+    def test_index(self):
+        '''
+        Test to exsure that the index page can be 
+        loaded with all the correct context elements 
+        that are expected to be present
+        '''
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('vendor_list' in resp.context)
+        self.assertEqual([vendor.pk for vendor in resp.context['vendor_list']], [1])
+        vendor1 = resp.context['vendor_list'][0]
+        self.assertEqual(vendor1.name, 'Dell Inc.') 
 
 
-class InventoryTestAttributesCase(TestCase):
-    # Perhaps we should consider http://docs.python.org/library/functions.html#hasattr
-    # for these simple tests
-    fixtures = ['ds']
-    def setUp(self):
-        call_command('loaddata', 'inventory')
-        self.vendor = Util.new_vendor(name='Vendorino', website='http://venderino.com')
-        self.datastore1 = Util.new_datastore("Storino")
-        self.server1 = Util.new_server('Serverino', self.vendor)
-
-    def testVendorAttributes(self):
-        self.assertEqual(self.vendor.name, "Vendorino")
-
-    def testServerAttributes(self):
-        self.assertEqual(self.server1.name, "Serverino")
-        self.assertEqual(self.server1.capacityMB, CAPACITYMB)
-        self.assertEqual(self.server1.vendor, self.vendor)
-        self.assertEqual(self.server1.purchased, DATETIME)
-
-    def testDataStoreAttributes(self):
-        self.assertEqual(self.datastore1.name, "Storino")
-        self.assertEqual(self.datastore1.capacityMB, CAPACITYMB)
-        self.assertEqual(self.datastore1.freespaceMB, FREESPACEMB)
-        self.assertEqual(self.datastore1.filesystemVersion, "3.14")
+    def test_datastores(self):
+        count = len(DataStore.objects.all())
+        assert(count == 2)
